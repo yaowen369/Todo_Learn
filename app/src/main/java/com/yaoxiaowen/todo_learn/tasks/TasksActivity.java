d413d08041494f1dd3445b276c34e43f6097cd1f
yaowen <yaowen369@163.com>
2018-03-14 13:40:26 +0800

    初始化工程

---
+++ app/src/main/java/com/yaoxiaowen/todo_learn/tasks/TasksActivity.java
@@ -0,0 +1,15 @@
+package com.yaoxiaowen.todo_learn.tasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yaoxiaowen.todo_learn.R;
+
+public class TasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tasks_act);
+    }
+}
